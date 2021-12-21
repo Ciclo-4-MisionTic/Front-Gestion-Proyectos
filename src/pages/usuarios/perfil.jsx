f94dbfe577f5d4795fec7b4fb95009dec27bc3f7
Petitevale <vrestrepol@unal.edu.co>
2021-12-21 02:17:39 -0500

    Se sabe

--- src/pages/usuarios/perfil.jsx
+++ src/pages/usuarios/perfil.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const perfil = () => {
-    return (
-        <div className='titulo'>
-            Aca va tu perfil
-        </div>
-    )
-}
+// const perfil = () => {
+//     return (
+//         <div className='titulo'>
+//             Aca va tu perfil
+//         </div>
+//     )
+// }
 
-export default perfil
+// export default perfil
